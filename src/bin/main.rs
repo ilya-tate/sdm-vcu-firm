d01b06870a4d917f5098508e96017dd1bedd9541
ilya-tate <ilyatate@gmail.com>
2026-09-09 00:45:15 -0700

    Removed unnecessary comments

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -15,7 +15,6 @@ use log::info;
 
 extern crate alloc;
 
-// This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -25,9 +24,6 @@ esp_bootloader_esp_idf::esp_app_desc!();
 )]
 #[main]
 fn main() -> ! {
-    // generator version: 1.3.0
-    // generator parameters: --chip esp32c6 -o unstable-hal -o alloc -o esp-backtrace -o log -o ci -o neovim -o vscode -o stable-x86_64-unknown-linux-gnu
-
     esp_println::logger::init_logger_from_env();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
@@ -40,6 +36,4 @@ fn main() -> ! {
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.1.0/examples
 }
