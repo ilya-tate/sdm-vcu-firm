16ae1d5c227750695163a4fc9d479dfb4e84e236
ilya-tate <ilyatate@gmail.com>
2026-09-12 18:34:09 -0700

    Started CAN implementation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -11,7 +11,9 @@ use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
 use esp_hal::main;
 use esp_hal::time::{Duration, Instant};
+//use log::{info, warn};
 use log::info;
+use esp_hal::twai::BaudRate;
 
 extern crate alloc;
 
@@ -27,13 +29,32 @@ fn main() -> ! {
     esp_println::logger::init_logger_from_env();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
 
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 65536);
 
+    // CAN 2.0 IO stuff
+    let _can3_stb = vcu_firm::can::tranceiver_enable(peripherals.GPIO4);
+    let (mut can3_rx, mut can3_tx) = vcu_firm::can::can_init(
+        // TODO:
+        // Verify bus speed value
+        peripherals.TWAI0,  // TWAI0
+        peripherals.GPIO14, // RX
+        peripherals.GPIO13, // TX
+        BaudRate::B500K     // BUS Speed
+    );
+
     loop {
-        info!("Hello world!");
         let delay_start = Instant::now();
+        info!("main.rs initializing...");
+    
+        // Can input checker
+        match can3_rx.receive() {
+            Ok(frame) => info!("CAN Frame: {frame}"),
+            Err(nb::Error::Other(e)) => info!("Can Frame Error: {e}"),
+            Err(nb::Error::WouldBlock) => {}
+        }
+
         while delay_start.elapsed() < Duration::from_millis(500) {}
     }
 }
---
+++ src/can.rs
@@ -0,0 +1,27 @@
+use esp_hal::gpio::{Level, InputPin, Output, OutputPin, OutputConfig};
+use esp_hal::twai::{BaudRate, TwaiRx, TwaiTx, TwaiConfiguration, TwaiMode};
+
+pub fn tranceiver_enable(standby_pin: impl OutputPin) -> Output<'static> {
+    Output::new(
+        standby_pin,
+        Level::Low, // Set polarity low
+        OutputConfig::default()
+    )
+}
+
+
+// I thought about using async, but am hesitant (bottlenecks)
+// Blocking could result in 'packet' loss (yes it says blocking, unrelated)
+// Im just gonna do a check in bin/main.rs,
+// it seems like the best of both worlds
+pub fn can_init(
+    twai0:      esp_hal::peripherals::TWAI0,
+    pin_rx:     impl InputPin,
+    pin_tx:     impl OutputPin,
+    baud_rate:  BaudRate
+) -> (TwaiTx<'static, esp_hal::Blocking>, TwaiRx<'static, esp_hal::Blocking>) {
+    TwaiConfiguration::new(twai0, pin_rx, pin_tx, baud_rate, TwaiMode::Normal)
+        .start()
+        .split()
+}
+
